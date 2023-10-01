69e9da2a405a731a725ef39ad18a4a7e24519dbc
Jucci16 <John.Ucci3@gmail.com>
2023-10-01 18:40:29 -0400

    Two Crystal Balls Completed.

--- Core/TwoCrystalBalls/TwoCrystalBalls.cs
+++ Core/TwoCrystalBalls/TwoCrystalBalls.cs
@@ -9,6 +9,26 @@ public static class TwoCrystalBalls
 {
     public static int MaxHeight(bool[] levels)
     {
-        throw new NotImplementedException();
+        var stepDistance = (int)Math.Floor(Math.Sqrt(levels.Length));
+        var level = 0;
+
+        while(level < levels.Length)
+        {
+            if (levels[level])
+            {
+                level -= stepDistance;
+                break;
+            }
+
+            level += stepDistance;
+        }
+
+        if (level > levels.Length) level -= stepDistance;
+        for (var i = level; i < levels.Length; ++i)
+        {
+            if (levels[i]) return i;
+        }
+
+        return -1;
     }
 }
