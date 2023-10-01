09304fb74256fcc759f2200e63c70178e6f60e15
Jucci16 <John.Ucci3@gmail.com>
2023-10-01 18:47:17 -0400

    Bubble Sort Completed.

--- Core/BubbleSort/BubbleSort.cs
+++ Core/BubbleSort/BubbleSort.cs
@@ -9,6 +9,22 @@ public static class BubbleSort
 {
     public static int[] Sort(int[] numbers)
     {
-        throw new NotImplementedException();
+        for (var i = 0; i < numbers.Length; ++i)
+        {
+            for (var j = i + 1; j < numbers.Length; ++j)
+            {
+                var a = numbers[i];
+                var b = numbers[j];
+
+                if (b < a)
+                {
+                    var temp = a;
+                    numbers[i] = numbers[j]; 
+                    numbers[j] = temp;
+                }
+            }
+        }
+
+        return numbers;
     }
 }
