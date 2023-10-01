8af50ad9369947b8d7d85b2991a0197031e7f172
Jucci16 <John.Ucci3@gmail.com>
2023-10-01 18:18:09 -0400

    Binary Search Completed.

--- Core/BinarySearch/BinarySearch.cs
+++ Core/BinarySearch/BinarySearch.cs
@@ -9,7 +9,27 @@ public static class BinarySearch
 {
     public static bool Search(int[] haystack, int needle)
     {
-        throw new NotImplementedException();
+        var min = 0;
+        var max = haystack.Length;
+
+        while (min < max)
+        {
+            var pivot = CalculatePivot(min, max);
+            var value = haystack[pivot];
+            if (needle == value) return true;
+
+            if (needle > value)
+                min = pivot + 1;
+            else
+                max = pivot;
+        }
+
+        return false;
+    }
+
+    private static int CalculatePivot(int min, int max)
+    {
+        return (max - min) / 2 + min;
     }
 
 }
