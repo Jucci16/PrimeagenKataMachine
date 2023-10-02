bed4ad2ebc614b8d95e6e29a6fd73cad6d85e9e0
Jucci16 <John.Ucci3@gmail.com>
2023-10-02 17:52:20 -0400

    Completed Compare Binary Search Tree.

--- Core/CompareBinaryTrees/CompareBinaryTrees.cs
+++ Core/CompareBinaryTrees/CompareBinaryTrees.cs
@@ -3,6 +3,12 @@ public static class CompareBinaryTrees
 {
     public static bool Compare(BinaryNode<int> a, BinaryNode<int> b)
     {
-        throw new NotImplementedException();
+        if (a is null || b is null) return a == b;
+
+        if (a.Value != b.Value) return false;
+        if (!Compare(a.Left, b.Left)) return false;
+        if (!Compare(a.Right, b.Right)) return false;
+
+        return true;
     }
 }
