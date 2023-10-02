849ff52d38d68225c7a2876031e492eaee93ab07
Jucci16 <John.Ucci3@gmail.com>
2023-10-02 17:48:36 -0400

    Added Binary Trees Test.

---
+++ KataMachine/CompareBinaryTreeTests.cs
@@ -0,0 +1,12 @@
+﻿using Core.CompareBinaryTrees;
+
+namespace KataMachine;
+public class CompareBinaryTreeTests
+{
+    [Fact]
+    public void ComparyBinaryTreesTest()
+    {
+        Assert.True(CompareBinaryTrees.Compare(Utilities.GenerateTree1(), Utilities.GenerateTree1()));
+        Assert.False(CompareBinaryTrees.Compare(Utilities.GenerateTree1(), Utilities.GenerateTree2()));
+    }
+}
