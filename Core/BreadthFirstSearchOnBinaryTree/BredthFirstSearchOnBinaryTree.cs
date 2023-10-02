a8d23eac5548e6d58243540a7bf68e30ea76a21e
Jucci16 <John.Ucci3@gmail.com>
2023-10-02 17:41:49 -0400

    Breadth First Search Completed.

--- Core/BreadthFirstSearchOnBinaryTree/BredthFirstSearchOnBinaryTree.cs
+++ Core/BreadthFirstSearchOnBinaryTree/BredthFirstSearchOnBinaryTree.cs
@@ -3,6 +3,17 @@ public static class BredthFirstSearchOnBinaryTree
 {
     public static bool Search(BinaryNode<int> node, int needle)
     {
-        throw new NotImplementedException();
+        var queue = new Queue<BinaryNode<int>>();
+        queue.Enqueue(node);
+
+        while (queue.Count > 0)
+        {
+            var binaryNode = queue.Dequeue();
+            if (binaryNode.Value == needle) return true;
+            if (binaryNode.Left is not null) queue.Enqueue(binaryNode.Left);
+            if (binaryNode.Right is not null) queue.Enqueue(binaryNode.Right);
+        }
+
+        return false;
     }
 }
