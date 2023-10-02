3a48ed2255fca4256e14f9f0cf0a0fde6ec988d9
Jucci16 <John.Ucci3@gmail.com>
2023-10-02 17:28:18 -0400

    Quick Sort Completed.

--- Core/QuickSort/QuickSort.cs
+++ Core/QuickSort/QuickSort.cs
@@ -9,6 +9,44 @@ public static class QuickSort
 {
     public static int[] Sort(int[] numbers)
     {
-        throw new NotImplementedException();
+        return SortArray(numbers, 0, numbers.Length - 1);
+    }
+
+    public static int[] SortArray(int[] numbers, int minIndex, int maxIndex)
+    {
+        if (minIndex >= maxIndex) return numbers;
+
+        var pivotIndex = Partition(numbers, minIndex, maxIndex);
+        SortArray(numbers, minIndex, pivotIndex - 1);
+        SortArray(numbers, pivotIndex + 1, maxIndex);
+
+        return numbers;
+    }
+
+    public static int Partition(int[] numbers, int min, int max)
+    {
+        var pivot = numbers[max];
+        var index = min - 1;
+
+        for (var i = min; i < max; ++i)
+        {
+            if (numbers[i] <= pivot)
+            {
+                index++;
+                Swap(numbers, index, i);
+            }
+        }
+
+        index++;
+        Swap(numbers, index, max);
+
+        return index;
+    }
+
+    public static void Swap(int[] numbers, int indexA, int indexB)
+    {
+        var temp = numbers[indexA]; 
+        numbers[indexA] = numbers[indexB];
+        numbers[indexB] = temp;
     }
 }
